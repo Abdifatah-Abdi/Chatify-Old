05c2a1b78e8da9d18c7d62ee41803e5397c8b10f
ClounZ <omarbassemelyossef@gmail.com>
2023-08-19 10:14:48 +0400

    Added timestamps to chat messages.

--- src/methods.js
+++ src/methods.js
@@ -18,28 +18,28 @@ export const delay = (ms) => {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export function convertTimeToUserTimezone(timeString) {
-	// Parse the input time string
-	let [time, ampm] = timeString.split(" ");
-	let [hours, minutes] = time.split(":");
+// export function convertTimeToUserTimezone(timeString) {
+// 	// Parse the input time string
+// 	let [time, ampm] = timeString.split(" ");
+// 	let [hours, minutes] = time.split(":");
 
-	// Convert hours and minutes to numbers
-	hours = parseInt(hours);
-	minutes = parseInt(minutes);
+// 	// Convert hours and minutes to numbers
+// 	hours = parseInt(hours);
+// 	minutes = parseInt(minutes);
 
-	// Get the user's local timezone
-	let userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// 	// Get the user's local timezone
+// 	let userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-	// Create a date object in the user's local timezone
-	let userDate = new Date();
+// 	// Create a date object in the user's local timezone
+// 	let userDate = new Date();
 
-	// Set hours, minutes, and seconds to match the input time
-	userDate.setHours(ampm === "am" ? hours : hours + 12);
-	userDate.setMinutes(minutes);
+// 	// Set hours, minutes, and seconds to match the input time
+// 	userDate.setHours(ampm === "am" ? hours : hours + 12);
+// 	userDate.setMinutes(minutes);
 
-	// Convert the user's local time to the user's timezone
-	let options = { timeZone: userTimezone, hour: "numeric", minute: "numeric" };
-	let userTime = userDate.toLocaleTimeString("en-US", options);
+// 	// Convert the user's local time to the user's timezone
+// 	let options = { timeZone: userTimezone, hour: "numeric", minute: "numeric" };
+// 	let userTime = userDate.toLocaleTimeString("en-US", options);
 
-	return userTime;
-};
+// 	return userTime;
+// };
