1496af2685e2bfbb3bcee9ee7e4701680ec9e91a
Abdifatah Abdi <abdiabdifatah030@gmail.com>
2023-08-19 00:31:49 -0400

    Added ability to delete messages

---
+++ src/signup.js
@@ -0,0 +1,5 @@
+const username = document.getElementById('signup-username');
+const email = document.getElementById('signup-email');
+const password = document.getElementById('signup-password');
+const confirm_password = document.getElementById('confirm-password');
+const signup_button = document.getElementById('')
