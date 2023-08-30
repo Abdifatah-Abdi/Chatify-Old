a8c3dd7dd07149945c31f60747a96abe21639e90
Omar <omarbassemelyossef@gmail.com>
2023-08-30 18:23:49 +0400

    Made navbar work on all pages connected to it. Started settings page
    So now you can simply just copy paste the profile menu and navbar html code into any html file, then link the css and js navbar files to this html file and done, you added the navbar to this page!
    
    Also, I've started work on the settings page, you can open it through the navbar (will remove later), or the profile menu if you're logged in.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import { deleteCookie, getCookie, authorization } from "./methods.js";
 
 const downloadPlatformSpan = document.getElementById("download-platform");
 const toTopButton = document.getElementById("to-top-button");
-const signButton = document.getElementById('login-button');
-const pfpIcon = document.getElementById("login-button").querySelector('img');
-const email_item = document.getElementById("email-item");
-const profile_menu = document.getElementById("profile-menu");
-
-
-const userInfo = {
-    username: '',
-    pfp: '',
-    email: '',
-};
 
 toTopButton.addEventListener("mouseup", () => {
     window.scrollTo({ top: 0, behavior: 'smooth', })
@@ -31,42 +20,3 @@ if (navigator.userAgent.indexOf("Windows") != -1) {
 } else {
     downloadPlatformSpan.textContent = `Huh?`;
 };
-
-console.log('Cookie: ', getCookie('id'));
-if (getCookie('id')) {
-    //get user information
-    const userResponse = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
-		method: "GET",
-		headers: {
-			"Authorization": authorization,
-		},
-	});
-	const usersData = await userResponse.json();
-
-	for (let index = 0; index < usersData.records.length; index++) {
-		const record = usersData.records[index];
-
-		if (record.fields.user_id == getCookie('id')) {
-            userInfo.username = record.fields.username;
-            userInfo.pfp = record.fields.pfp_link;
-            userInfo.email = record.fields.email;
-        }
-	};
-
-    signButton.querySelector('p').textContent = userInfo.username
-    signButton.setAttribute('href', '#')
-    pfpIcon.style.display = 'block'
-    email_item.textContent = userInfo.email;
-}
-
-signButton.addEventListener('mouseup', () => {
-    if (signButton.getAttribute('href') != '#') {
-        console.log(signButton.getAttribute('href'));
-    } else {
-        profile_menu.style.setProperty('display', profile_menu.style.display === 'none' ? 'block' : 'none');
-    }
-})
-
-profile_menu.lastElementChild.addEventListener('mouseup', () => {
-    deleteCookie('id');
-});
