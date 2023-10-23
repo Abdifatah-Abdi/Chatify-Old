db7a8440ce6a649893af7afaaecd205209a96690
Advay Chandorkar <advay1612@icloud.com>
2023-10-23 14:35:45 -0400

    bring back account limitation

--- src/js/login.js
+++ src/js/login.js
@@ -1,92 +1,58 @@
-import { authorization, random, setInfiniteCookie } from "./methods.js";
-
-//#region Elements
-//#region Containers, Selectors, and Button elements
+import { delay, getCookie, authorization } from "./methods.js";
+// DOM Elements
 const signInContainer = document.getElementById("sign-in-container");
 const signUpContainer = document.getElementById("sign-up-container");
 const signInSelector = document.getElementById("sign-in-selector");
 const signUpSelector = document.getElementById("sign-up-selector");
 const signInButton = document.getElementById("sign-in-button");
 const signUpButton = document.getElementById("sign-up-button");
-//#endregion
-//#region Sign-up Elements
-const createUsernameInput = document.getElementById("create-username-input");
-const createEmailInput = document.getElementById("create-email-input");
-const createPasswordInput = document.getElementById("create-password-input");
+// Sign-up Elements
+const usernameInput = document.getElementById("create-username-input");
+const emailInput = document.getElementById("create-email-input");
+const passwordInput = document.getElementById("create-password-input");
 const confirmPasswordInput = document.getElementById("create-confirm-password-input");
-//#endregion
-//#region Sign-in Elements
+const subText = document.getElementsByTagName('h2');
+// Sign-in Elements
 const loginEmailInput = document.getElementById("login-email-input");
 const loginPasswordInput = document.getElementById("login-password-input");
 const rememberBox = document.getElementById('remember-me');
-//#endregion
-//#region Error Elements
-const takenUsernameError = document.getElementById("taken-username-error");
-const availableUsername = document.querySelectorAll(".available-username");
-const takenEmailError = document.getElementById("taken-email-error");
-const nonMatchingPasswordError = document.getElementById("non-matching-password-error");
-const fillInFormsError = document.getElementById("fill-in-all-forms-error");
-
-const incorrectCredentialsError = document.getElementById("incorrect-credentials-error");
-const fillInCredentialsError = document.getElementById("fill-in-all-credentials-error");
-//#endregion
-//#endregion
-//#region Event Listeners
-signInSelector.addEventListener("mouseup", signInSelectorClick);
-signUpSelector.addEventListener("mouseup", signUpSelectorClick);
-signInButton.addEventListener('mouseup', signInButtonOnClick);
-signUpButton.addEventListener("mouseup", signUpButtonOnClick);
-createUsernameInput.addEventListener("focusout", createUsernameInputFocusOut);
-availableUsername.forEach(availableUsernameLoop);
-//#endregion
-//#region Functions
-async function signInButtonOnClick() {
-    const userData = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
-        method: "GET",
-        headers: {
-            "Authorization": authorization,
-            "Content-Type": "application/json",
-        },
-    });
-    const parsedData = await userData.json();
-
-    if (!loginEmailInput.value || !loginPasswordInput.value) {
-        fillInCredentialsError.classList.remove('hidden');
-        errorEffect(loginEmailInput);
-        errorEffect(loginPasswordInput);
-        return;
-    };
-
-    for (let index = 0; index < parsedData.records.length; index++) {
-        const record = parsedData.records[index];
-
-        if (!(loginEmailInput.value === record.fields.email && loginPasswordInput.value === record.fields.password ||
-            loginEmailInput.value === record.fields.username && loginPasswordInput.value === record.fields.password))
-            continue;
-
-        undoErrorEffect(loginEmailInput);
-        undoErrorEffect(loginPasswordInput);
-
-        setInfiniteCookie("id", record.fields.user_id);
-        window.location.href = "./chat.html";
-        return;
-    };
-
-    incorrectCredentialsError.classList.remove("hidden");
-    loginPasswordInput.value = "";
-
-    errorEffect(loginEmailInput);
-    errorEffect(loginPasswordInput);
-};
-
-async function signUpButtonOnClick() {
-    if (!createUsernameInput.value || !createEmailInput.value || !createPasswordInput.value || !confirmPasswordInput.value) {
-        fillInFormsError.classList.remove("hidden");
-        return;
-    } else {
-        fillInFormsError.classList.add("hidden");
-    };
-
+// Error Effect
+async function ErrorEffect(inputForm, errorText, subIndex) {
+    subText[subIndex].style.color = '#ff5f36';
+    subText[subIndex].textContent = '*' + errorText;
+    subText[subIndex].style.opacity = 1;
+    if (inputForm) {
+        inputForm.classList.add('error');
+    }
+}
+// Success Effect
+async function SuccessEffect(successText, subIndex) {
+    subText[subIndex].style.color = 'lime';
+    subText[subIndex].textContent = successText;
+    subText[subIndex].style.opacity = 1;
+}
+// Validate Email
+async function ValidateEmail(emailForm) {
+    const validRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return validRegex.test(emailForm.value);
+}
+// Event Listeners
+signInSelector.addEventListener("mouseup", () => {
+    signInSelector.classList.add("active-selector");
+    signUpSelector.classList.remove("active-selector");
+    signInContainer.classList.remove("hidden-login-container");
+    signUpContainer.classList.add("hidden-login-container");
+});
+signUpSelector.addEventListener("mouseup", () => {
+    signInSelector.classList.remove("active-selector");
+    signUpSelector.classList.add("active-selector");
+    signInContainer.classList.add("hidden-login-container");
+    signUpContainer.classList.remove("hidden-login-container");
+    subText[1].style.color = 'white';
+    subText[1].textContent = "Create a free account to use Chatify's services!";
+    subText[1].style.opacity = 0.6;
+});
+signUpButton.addEventListener("click", async () => {
     const userData = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
         method: "GET",
         headers: {
@@ -95,49 +61,52 @@ async function signUpButtonOnClick() {
         },
     });
     const users = await userData.json();
-
-    for (let index = 0; index < users.records.length; index++) {
-        const record = users.records[index];
-
-        if (record.fields.username === createUsernameInput.value) {
-            errorEffect(createUsernameInput);
-            takenUsernameError.classList.remove("hidden");
-
-            availableUsername.forEach(element => element.textContent = `${createUsernameInput.value}${random(20, 100)}`);
+    for (let i = 0; i < users.records.length; i++) {
+        const user = users.records[i];
+        if (!usernameInput.value || !emailInput.value || !passwordInput.value || !confirmPasswordInput.value) {
+            ErrorEffect(null, 'Fill in all the forms', 1);
             return;
-        } else {
-            undoErrorEffect(createUsernameInput);
-            takenUsernameError.classList.add("hidden");
-        };
+        }
 
-        if (record.fields.email === createEmailInput.value) {
-            errorEffect(createEmailInput);
-            takenEmailError.classList.remove("hidden");
+        // Username validation
+        const usernameRegex = /^[a-zA-Z0-9_]+$/; // Regular expression to match letters, numbers, and underscores
+        if (!usernameRegex.test(usernameInput.value)) {
+            ErrorEffect(usernameInput, "Username can only contain letters, numbers, and underscores", 1);
             return;
-        } else {
-            undoErrorEffect(createEmailInput);
-            takenEmailError.classList.add("hidden");
-        };
+        }
 
-        if (createPasswordInput.value !== confirmPasswordInput.value) {
-            errorEffect(createPasswordInput);
-            errorEffect(confirmPasswordInput);
-
-            nonMatchingPasswordError.classList.remove("hidden");
-
-            createPasswordInput.value = "";
-            confirmPasswordInput.value = "";
+        if (user.fields.username === usernameInput.value) {
+            ErrorEffect(usernameInput, "This username is taken!", 1);
             return;
-        } else {
-            undoErrorEffect(createPasswordInput);
-            undoErrorEffect(confirmPasswordInput);
-
-            nonMatchingPasswordError.classList.add("hidden");
-        };
-    };
-
+        }
+        // Password validation for length
+        if (passwordInput.value.length < 5) {
+            ErrorEffect(passwordInput, 'Your password must be longer than 5 characters', 1);
+            return;
+        }
+        // Password validation for uppercase letter and number
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).+$/;
+        if (!passwordRegex.test(passwordInput.value)) {
+            ErrorEffect(passwordInput, 'Password must include an uppercase letter and a number', 1);
+            return;
+        }
+        if (!ValidateEmail(emailInput)) {
+            ErrorEffect(emailInput, "Invalid email format!", 1);
+            return;
+        }
+        if (user.fields.email === emailInput.value) {
+            ErrorEffect(emailInput, "Unoriginal email", 1);
+            return;
+        }
+    }
+    if (passwordInput.value !== confirmPasswordInput.value) {
+        ErrorEffect(confirmPasswordInput, "Passwords do not match", 1);
+        passwordInput.value = "";
+        confirmPasswordInput.value = "";
+        return;
+    }
     const maxUserId = await getMaxUserId();
-    await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
+    const response = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
         method: "POST",
         headers: {
             "Authorization": authorization,
@@ -146,57 +115,21 @@ async function signUpButtonOnClick() {
         body: JSON.stringify({
             fields: {
                 user_id: maxUserId,
-                username: createUsernameInput.value,
+                username: usernameInput.value,
                 date_created: Date.now(),
-                email: createEmailInput.value,
-                password: createPasswordInput.value,
+                email: emailInput.value,
+                password: passwordInput.value,
             },
         }),
     });
+    const parsedResponse = await response.json();
     document.cookie = `id=${maxUserId};expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-};
-
-async function createUsernameInputFocusOut() {
-    const userData = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
-        method: "GET",
-        headers: {
-            "Authorization": authorization,
-            "Content-Type": "application/json",
-        },
-    });
-    const users = await userData.json();
-
-    for (let index = 0; index < users.records.length; index++) {
-        const record = users.records[index];
-
-        if (record.fields.username === createUsernameInput.value) {
-            errorEffect(createUsernameInput);
-            takenUsernameError.classList.remove("hidden");
-            fillInFormsError.classList.add("hidden");
-
-            availableUsername.forEach(element => element.textContent = `${createUsernameInput.value}${random(20, 100)}`);
-            return;
-        } else {
-            undoErrorEffect(createUsernameInput);
-            takenUsernameError.classList.add("hidden");
-        };
-    };
-};
-
-function signInSelectorClick() {
-    signInSelector.classList.add("active-selector");
-    signUpSelector.classList.remove("active-selector");
-    signInContainer.classList.remove("hidden-login-container");
-    signUpContainer.classList.add("hidden-login-container");
-};
-
-function signUpSelectorClick() {
-    signInSelector.classList.remove("active-selector");
-    signUpSelector.classList.add("active-selector");
-    signInContainer.classList.add("hidden-login-container");
-    signUpContainer.classList.remove("hidden-login-container");
-};
-
+    console.log(getCookie('id'));
+    SuccessEffect('Account created!', 1);
+    await delay(1000);
+    window.location.href = 'chat.html';
+});
+// Get highest user id available
 async function getMaxUserId() {
     const userData = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
         method: "GET",
@@ -206,32 +139,46 @@ async function getMaxUserId() {
         },
     });
     const users = await userData.json();
-
     let max = 0;
     for (const user of users.records) {
         const user_id = user.fields.user_id;
-
-        if (user_id > max)
+        if (user_id > max) {
             max = user_id;
-    };
-
+        }
+    }
     return ++max;
-};
-
-async function errorEffect(inputForm) {
-    inputForm.classList.add('error');
-};
-
-async function undoErrorEffect(inputForm) {
-    inputForm.classList.remove('error');
-};
-
-function availableUsernameLoop(element) {
-    element.addEventListener("mouseup", availableUsernameElementClick.bind(null, element));
-};
-
-function availableUsernameElementClick(element) {
-    createUsernameInput.value = element.textContent;
-    undoErrorEffect(createUsernameInput);
-};
-//#endregion
+}
+// Sign-in section
+signInButton.addEventListener('mouseup', async () => {
+    const expires = rememberBox.checked ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "";
+    const userData = await fetch("https://api.airtable.com/v0/appDfdVnrEoxMyFfF/Users", {
+        method: "GET",
+        headers: {
+            "Authorization": authorization,
+            "Content-Type": "application/json",
+        },
+    });
+    const users = await userData.json();
+    if (!loginEmailInput.value || !loginPasswordInput.value) {
+        ErrorEffect(null, "Please fill in all forms", 0);
+        return;
+    }
+    let foundUser = null;
+    if (await ValidateEmail(loginEmailInput)) {
+        foundUser = users.records.find(user => user.fields.email === loginEmailInput.value);
+    } else {
+        foundUser = users.records.find(user => user.fields.username === loginEmailInput.value);
+    }
+    if (foundUser) {
+        if (foundUser.fields.password === loginPasswordInput.value) {
+            SuccessEffect("Found your account!", 0);
+            document.cookie = `id=${foundUser.fields.user_id}${expires};`
+            await delay(1000);
+            window.location.href = 'chat.html';
+        } else {
+            ErrorEffect(loginPasswordInput, "Password incorrect!", 0);
+        }
+    } else {
+        ErrorEffect(loginEmailInput, "Couldn't find account", 0);
+    }
+});
